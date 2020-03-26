f79a16af5f8663bb0d26e9cb2936aedd8f4411cc
hrutvik <hrutvikpatel@hotmail.com>
2020-03-26 19:18:03 -0400

    Updated to incorporate safest path compatibility

--- app/src/main/java/com/example/lorawanfiresensor/SafetyActivity.java
+++ app/src/main/java/com/example/lorawanfiresensor/SafetyActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,17 +27,33 @@ public class SafetyActivity extends Activity {
     // message comes in the form {"temperature":XX,"humidity":XX}
     //with algo future msgs will be {"temperature":XX,"humidity":XX, "direction":XX}
 
-
-    static String MQTTHOST= "XXXXXXX";
-    static String USERNAME= "XXXXXX";
-    static String PASSWORD= "XXXXXXX";
-    String topicStr="Sensor/Data";
+    static String MQTTHOST= "tcp://hairdresser.cloudmqtt.com:15955";
+    static String USERNAME= "pslgynex";
+    static String PASSWORD= "MOCo261u5hxW";
+    String topicStr="Algorithm/Data"; //change topic to Algorithm/Data
 
     MqttAndroidClient client;
 
     TextView mtvDirection;
     ImageButton mibtnPubLocation;
     ImageButton mibtnPubSafety;
+
+    //ImageView ivBlueprint= (ImageView)findViewById(R.id.ivBlueprint);
+    ImageView A ;
+    ImageView B ;
+    ImageView C ;
+    ImageView D ;
+    ImageView E ;
+    ImageView F ;
+    ImageView G ;
+    ImageView H ;
+    ImageView I ;
+    ImageView J ;
+    ImageView K ;
+
+
+
+
 
     MqttConnectOptions options;
 
@@ -62,10 +79,35 @@ public class SafetyActivity extends Activity {
         setContentView(R.layout.activity_safety); //first XML
 
 
-       mibtnPubLocation =(ImageButton)findViewById(R.id.ibtnPubLocation);
-        mibtnPubSafety =(ImageButton)findViewById(R.id.ibtnPubSafety);
+   //    mibtnPubLocation =(ImageButton)findViewById(R.id.ibtnPubLocation);
+   //     mibtnPubSafety =(ImageButton)findViewById(R.id.ibtnPubSafety);
 //        mtvDirection= (TextView)findViewById(R.id.tvDirection);
 
+        //ivBlueprint.setVisibility(View.VISIBLE);
+
+         A= (ImageView)findViewById(R.id.A);
+         B= (ImageView)findViewById(R.id.B);
+         C= (ImageView)findViewById(R.id.C);
+         D= (ImageView)findViewById(R.id.D);
+         E= (ImageView)findViewById(R.id.E);
+         F= (ImageView)findViewById(R.id.F);
+         G= (ImageView)findViewById(R.id.G);
+         H= (ImageView)findViewById(R.id.H);
+         I= (ImageView)findViewById(R.id.I);
+         J= (ImageView)findViewById(R.id.J);
+         K= (ImageView)findViewById(R.id.K);
+
+        A.setVisibility(View.INVISIBLE);
+        B.setVisibility(View.INVISIBLE);
+        C.setVisibility(View.INVISIBLE);
+        D.setVisibility(View.INVISIBLE);
+        E.setVisibility(View.INVISIBLE);
+        F.setVisibility(View.INVISIBLE);
+        G.setVisibility(View.INVISIBLE);
+        H.setVisibility(View.INVISIBLE);
+        I.setVisibility(View.INVISIBLE);
+        J.setVisibility(View.INVISIBLE);
+        K.setVisibility(View.INVISIBLE);
 
         String clientId = MqttClient.generateClientId();
         client =
@@ -79,33 +121,34 @@ public class SafetyActivity extends Activity {
 
         deployMqtt();
 
-        mibtnPubLocation.setOnClickListener(new View.OnClickListener() {
-            /**
-             * This method is done when the user clicks the Node A button
-             *
-             * @param v
-             */
-            @Override
-            public void onClick(View v) {
-                pub("helpMe,location");
-                Toast.makeText(SafetyActivity.this,"Help is on the way! Your location has been sent", Toast.LENGTH_LONG).show();
+ //       mibtnPubLocation.setOnClickListener(new View.OnClickListener() {
+    //        /**
+    //         * This method is done when the user clicks the Node A button
+    //         *
+    //         * @param v
+     //        */
+   //         @Override
+   //         public void onClick(View v) {
+    //            pub("helpMe,location");
+    //            Toast.makeText(SafetyActivity.this,"Help is on the way! Your location has been sent", Toast.LENGTH_LONG).show();
 
-            }
-        });
+    //        }
+     //   });
 
-        mibtnPubSafety.setOnClickListener(new View.OnClickListener() {
-            /**
-             * This method is done when the user clicks the Node A button
-             *
-             * @param v
-             */
-            @Override
-            public void onClick(View v) {
-                pub("imSafe,name");
-                Toast.makeText(SafetyActivity.this,"You have been marked safe", Toast.LENGTH_LONG).show();
 
-            }
-        });
+    //    mibtnPubSafety.setOnClickListener(new View.OnClickListener() {
+    //        /**
+    //         * This method is done when the user clicks the Node A button
+    //         *
+     //        * @param v
+     //        */
+     //       @Override
+     //       public void onClick(View v) {
+      //          pub("imSafe,name");
+       //         Toast.makeText(SafetyActivity.this,"You have been marked safe", Toast.LENGTH_LONG).show();
+
+      //      }
+      //  });
 
 
 
@@ -151,14 +194,70 @@ public class SafetyActivity extends Activity {
             public void messageArrived(String topic, MqttMessage message) throws Exception {
                 String msg=new String(message.getPayload());
                 String [] data=  msg.split(",");
-                String []temperatureData=data[0].split(":");
-                String temperature=temperatureData[1];
+
+                //String []data= new String[] {"A","B","C","D","E"}; //for testing
 
 
-                String []humidityData=data[1].split(":");
-                String humidity=humidityData[1];
-                humidity = humidity.substring(0, humidity.length() - 1); //delete this when directions is implemented
-                 }
+
+                A.setVisibility(View.INVISIBLE);
+                B.setVisibility(View.INVISIBLE);
+                C.setVisibility(View.INVISIBLE);
+                D.setVisibility(View.INVISIBLE);
+                E.setVisibility(View.INVISIBLE);
+                F.setVisibility(View.INVISIBLE);
+                G.setVisibility(View.INVISIBLE);
+                H.setVisibility(View.INVISIBLE);
+                I.setVisibility(View.INVISIBLE);
+                J.setVisibility(View.INVISIBLE);
+                K.setVisibility(View.INVISIBLE);
+
+                for(int i=0;i<data.length;i++){
+                    if(data[i].equals("A")){
+                        A.setVisibility(View.VISIBLE);
+                    }
+                    if(data[i].equals("B")){
+                        B.setVisibility(View.VISIBLE);
+
+                    }
+                    if(data[i].equals("C")){
+                        C.setVisibility(View.VISIBLE);
+
+                    }
+                    if(data[i].equals("D")){
+                        D.setVisibility(View.VISIBLE);
+
+                    }
+                    if(data[i].equals("E")){
+                        E.setVisibility(View.VISIBLE);
+
+                    }
+                    if(data[i].equals("F")){
+                        F.setVisibility(View.VISIBLE);
+
+                    }
+                    if(data[i].equals("G")){
+                        G.setVisibility(View.VISIBLE);
+
+                    }
+                    if(data[i].equals("H")){
+                        H.setVisibility(View.VISIBLE);
+
+                    }
+                    if(data[i].equals("I")){
+                        I.setVisibility(View.VISIBLE);
+
+                    }
+                    if(data[i].equals("J")){
+                        J.setVisibility(View.VISIBLE);
+
+                    }
+                    if(data[i].equals("K")){
+                        K.setVisibility(View.VISIBLE);
+
+                    }
+                }
+
+            }
 
             @Override
             public void deliveryComplete(IMqttDeliveryToken token) {
@@ -169,3 +268,6 @@ public class SafetyActivity extends Activity {
 
     }
 }
+
+//change iv visibility:
+//ivBlueprint.setVisibility(View.VISIBLE);
